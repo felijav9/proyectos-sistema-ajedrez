17b8de5b46ff7287f5fc3a497f511a2520ac0132
felijav9 <felijav9@gmail.com>
2026-03-28 20:35:30 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Volt::route('/torneo-equipos-marzoddddd', 'torneo-emparejamientos')->name('torneo.index');
+// Volt::route('/torneo-equipos-marzoddddd', 'torneo-emparejamientos')->name('torneo.index');
 
-Volt::route('/registrar-resultados', 'registrar-resultados')->name('registrar.resultados');
+// Volt::route('/registrar-resultados', 'registrar-resultados')->name('registrar.resultados');
 
 
-Volt::route('/live-results-marzo2026ddddd', 'live-results-marzo2026')->name('live.results');
+// Volt::route('/live-results-marzo2026ddddd', 'live-results-marzo2026')->name('live.results');
 
 
 // Route::get('/torneo', TorneoEmparejamientos::class)->name('torneo.index');
